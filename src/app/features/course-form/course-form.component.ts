a032801087d898a71ad48c7790d3fe42c0bdeb60
Aseel <aseelmahmood12@gmail.com>
2026-02-22 21:08:25 +0100

    Add form validation

--- src/app/features/course-form/course-form.component.ts
+++ src/app/features/course-form/course-form.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ButtonComponent } from '../../shared/components/button/button.component';
 
 @Component({
   selector: 'app-course-form',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, ReactiveFormsModule],
   templateUrl: './course-form.component.html',
   styleUrl: './course-form.component.scss',
 })
-export class CourseFormComponent {}
+export class CourseFormComponent {
+  private fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    courseName: ['', Validators.required],
+    courseType: ['', Validators.required],
+    absenceLimit: [3, [Validators.required, Validators.min(0), Validators.max(100)]],
+    currentAbsenceCount: [0, [Validators.required, Validators.min(0), Validators.max(100)]],
+  });
+
+  get courseName() {
+    return this.form.get('courseName')!;
+  }
+
+  get courseType() {
+    return this.form.get('courseType')!;
+  }
+
+  get absenceLimit() {
+    return this.form.get('absenceLimit')!;
+  }
+
+  get currentAbsenceCount() {
+    return this.form.get('currentAbsenceCount')!;
+  }
+
+  onSubmit() {
+    if (this.form.invalid) {
+      this.form.markAllAsTouched();
+      return;
+    }
+    // add course
+  }
+}
